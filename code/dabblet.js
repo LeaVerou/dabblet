cb6f47db8e2b42bafedee60df600f7a01d159104
Lea Verou <lea@verou.me>
2012-01-06 17:56:53 +0000

    Refactor to make it more similar with labs branch

--- code/dabblet.js
+++ code/dabblet.js
@@ -1,19 +1,13 @@
-function $(expr, con) { return (con || document).querySelector(expr); }
-function $$(expr, con) { return [].slice.call((con || document).querySelectorAll(expr)); }
-
-// Webkit does this anyway
-$$('[id]').forEach(function(element) {
-	window[element.id] = element;
-});
-
 var gist = {
+	clientId: 'da931d37076424f332ef',
+	
 	oauth: [
 		// Step 1: Ask permission
 		function(callback){
 			gist.oauth.callback = callback;
 			
 			var popup = open('https://github.com/login/oauth/authorize' + 
-				'?client_id=da931d37076424f332ef' +
+				'?client_id=' + gist.clientId +
 				'&scope=gist', 'popup', 'width=1015,height=500');
 		},
 		// Step 2: Get access token and store it
@@ -49,7 +43,7 @@ var gist = {
 				(o.id? '/' + o.id : '') +
 				(o.gpath || '');
 		
-		xhr({
+		$u.xhr({
 			method: o.method,
 			url: 'https://api.github.com/' + path + (!o.anon && ACCESS_TOKEN? '?access_token=' + ACCESS_TOKEN : ''),
 			headers: o.headers,
@@ -275,143 +269,15 @@ Object.defineProperty(gist, 'saved', {
 	}
 });
 
-var UndoManager = function(elm) {
-	this.element = elm;
-	
-	this.undoStack = [];
-	this.redoStack = [];
-};
-
-UndoManager.prototype = {
-	action: function(action) {
-		if(!action || !(action.length || action.action || action.add || action.del)) {
-			return;
-		}
-		
-		var lastAction = this.undoStack.pop() || null;
-
-		if(lastAction) {
-			var push = lastAction.action || action.action
-					|| lastAction.length || action.length
-					|| (action.del && lastAction.add) 
-					|| (action.add && !lastAction.add)
-					|| (lastAction.start + lastAction.add.length - lastAction.del.length != action.start);
-			
-			if(push) {
-			  	this.undoStack.push(lastAction);
-			  	this.undoStack.push(action);
-			}
-			else if(lastAction) {
-				var combined = this.chain(lastAction, action);
-				
-				this.undoStack.push(combined);
-			}
-		}
-		else {
-			this.undoStack.push(action);
-		}
-		
-		this.redoStack = [];
-		
-		//console.log(this.undoStack);
-	},
-	
-	undo: function() {
-		//console.log(this.undoStack);
-		
-		var action = this.undoStack.pop();
-		
-		if(!action) {
-			return;
-		}
-		
-		this.redoStack.push(action);
-		
-		this.applyInverse(action);
-		
-		this.element.onkeyup();
-	},
-	
-	redo: function() {
-		//console.log(this.redoStack);
-		
-		var action = this.redoStack.pop();
-		
-		if(!action) {
-			return;
-		}
-		
-		this.undoStack.push(action);
-		
-		this.apply(action);
-		
-		this.element.onkeyup();
-	},
-	
-	chain: function(action1, action2) {
-		return {
-			add: action1.add + action2.add,
-			del: action2.del + action1.del,
-			start: action1.start
-		}
-	},
-	
-	apply: function(action) {
-		if(action.length) {
-			for(var i=0; i<action.length; i++) {
-				this.apply(action[i]);
-			}
-			return;
-		}
-		
-		var element = this.element,
-			start = action.start;
-			
-		if(action.action) {
-			Dabblet.codeActions.call(element, action.action, {
-				inverse: action.inverse,
-				start: start,
-				end: action.end,
-				noHistory: true
-			});
-		}
-		else {		
-			// add added chars & remove deleted chars			
-			element.textContent = element.textContent.splice(start, action.del.length, action.add);
-			
-			element.setSelectionRange(start, start + action.add.length);
-		}
-	},
-	
-	applyInverse: function(action) {
-		if(action.length) {
-			for(var i=action.length-1; i>=0; i--) {
-				this.applyInverse(action[i]);
-			}
-			return;
-		}
-		
-		var element = this.element,
-			start = action.start;
-		
-		if(action.action) {
-			Dabblet.codeActions.call(element, action.action, {
-				inverse: !action.inverse,
-				start: start,
-				end: action.end,
-				noHistory: true
-			});
-		}
-		else {
-			// remove added chars & add deleted chars
-			element.textContent = element.textContent.splice(start, action.add.length, action.del);
-			
-			element.setSelectionRange(start, start + action.del.length);
-		}
-	}
-};
-
 var Dabblet = {
+	version: '1.0.6',
+	
+	pages: {
+		css: window['css-page'],
+		html: window['html-page'], 
+		result: result
+	},
+	
 	title: function(code) {
 		return (code && code.match(/^\/\*[\s\*\r\n]+(.+?)($|\*\/)/m) || [,'Untitled'])[1];
 	},
@@ -431,10 +297,25 @@ var Dabblet = {
 		return false;
 	},
 	
+	get popup() {
+		return popup.src;
+	},
+	
+	set popup(url) {
+		if(url) {
+			popup.src = url;
+			popup.parentNode.style.display = 'block';
+		}
+		else {
+			popup.src = '';
+			popup.parentNode.style.display = '';
+		}
+	},
+	
 	update: {
 		CSS: function(code) {
 			if(!result.contentWindow.style) {
-				result.onload();
+				return;
 			}
 			
 			var style = result.contentWindow.style;
@@ -462,179 +343,6 @@ var Dabblet = {
 				result.contentDocument.body.innerHTML = code;
 			}
 		}
-	},
-	
-	codeActions: function(action, options) {
-		options = options || {};
-		
-		var text = this.textContent,
-			ss = options.start || this.selectionStart,
-			se = options.end || this.selectionEnd,
-			before = text.slice(0,ss),
-			after = text.slice(se),
-			selection = ss === se? '' : text.slice(ss,se),
-			textAction;
-		
-		switch (action) {
-		  case 'indent':
-		  	if(selection) {
-		  		var lf = before.lastIndexOf('\n') + 1;
-				
-				if(options.inverse) {
-					if(/\s/.test(before.charAt(lf))) {
-						before = before.splice(lf, 1);
-						
-						ss -= 1;
-						se -= 1;
-					}
-					
-					selection = selection.replace(/\r?\n\s/g, '\n');
-					var offset = selection.length - (se - ss);
-					
-					se += offset;
-				}
-				else {
-					before = before.splice(lf, 0, '\t');
-					selection = selection.replace(/\r?\n/g, '\n\t');
-					
-					var offset = selection.length - (se - ss);
-					
-					ss++;
-					se += offset + 1;
-				}
-				
-				textAction = {
-					action: action,
-					start: ss,
-					end: se,
-					inverse: options.inverse
-				};
-			}
-			else {
-				if(options.inverse) {
-					return false;
-				}
-				else {
-					textAction = {
-						add: '\t',
-						del: '',
-						start: ss
-					};
-					
-					before = before + '\t';
-					
-					ss++;
-					se++;
-				}
-			}
-			
-			break;
-			
-		  case 'newline':
-		  	var lf = before.lastIndexOf('\n') + 1,
-				indent = (before.slice(lf).match(/^\s+/) || [''])[0];
-			
-			textAction = {
-				add: '\n' + indent,
-				del: selection,
-				start: ss
-			};
-			
-			before += '\n' + indent;
-			selection = '';	
-			
-			ss += indent.length + 1;
-			se = ss;
-			
-			break;
-			
-		  case 'comment':
-			var css = this.id === 'css',
-				open = css? '/*' : '<!--',
-				close = css? '*/' : '-->';
-			
-			var start = before.lastIndexOf(open),
-				end = after.indexOf(close),
-				closeBefore = before.lastIndexOf(close),
-				openAfter = after.indexOf(start);
-				
-			if(start > -1 && end > -1
-			   	&& (start > closeBefore || closeBefore === -1)
-			   && (end < openAfter || openAfter === -1)
-			   ) {
-				// Uncomment
-				before = before.splice(start, open.length);
-				after = after.splice(end, close.length);
-				
-				textAction = [{
-					add: '',
-					del: open,
-					start: start
-				}, {
-					add: '',
-					del: close,
-					start: before.length + selection.length + end
-				}];
-				
-				ss -= open.length;
-				se -= open.length;
-			}
-			else {
-				// Comment
-				if(selection) {
-					// Comment selection
-					selection = open + selection + close;
-					
-					textAction = [{
-						add: open,
-						del: '',
-						start: ss
-					}, {
-						add: close,
-						del: '',
-						start: open.length + se
-					}];
-				}
-				else {
-					// Comment whole line
-					var start = before.lastIndexOf('\n') + 1,
-						end = after.indexOf('\n');
-					
-					if(end === -1) {
-						end = after.length;
-					}
-					
-					before = before.splice(start, 0, open);
-					
-					after = after.splice(end, 0, close);
-					
-					textAction = [{
-						add: open,
-						del: '',
-						start: start
-					}, {
-						add: close,
-						del: '',
-						start: before.length + end
-					}];
-				}
-				
-				ss += open.length;
-				se += open.length;
-			}
-			
-			break;
-		}
-				
-		this.textContent = before + selection + after;
-		
-		if(textAction && !options.noHistory) {
-			this.undoManager.action(textAction);
-		}
-		
-		this.setSelectionRange(ss, se);
-		
-		this.onkeyup();
 	},
 	
 	settings: {
@@ -760,7 +468,7 @@ var Dabblet = {
 		applyOne: function(name, value) {			
 			var current = this.current(name),
 				controls = document.getElementsByName(name);
-				
+
 			for(var i=0; i<controls.length; i++) {
 				var control = controls[i];
 				
@@ -893,251 +601,9 @@ document.addEventListener('DOMContentLoaded', function() {
 	}
 });
 
-$$('pre').forEach(function(pre){
-	pre.undoManager = new UndoManager(pre);
-	
-	pre.onkeydown = function(evt) {
-		var cmdOrCtrl = evt.metaKey || evt.ctrlKey;
-			
-		switch(evt.keyCode) {
-			case 8: // Backspace
-				var ss = this.selectionStart,
-					se = this.selectionEnd,
-					length = ss === se? 1 : Math.abs(se - ss),
-					start = se - length;
-				
-				this.undoManager.action({
-					add: '',
-					del: this.textContent.slice(start, se),
-					start: start
-				});
-				
-				break;
-			case 9: // Tab
-				if(!cmdOrCtrl) {
-					Dabblet.codeActions.call(this, 'indent', {
-						inverse: evt.shiftKey
-					});
-					return false;
-				}
-			case 13:
-				Dabblet.codeActions.call(this, 'newline');
-				return false;
-			case 90:
-				if(cmdOrCtrl) {
-					this.undoManager[evt.shiftKey? 'redo' : 'undo']();
-					return false;
-				}
-				
-				break;
-			case 191:
-				if(cmdOrCtrl) {
-					Dabblet.codeActions.call(this, 'comment');
-					return false;
-				}
-				
-				break;
-		}
-	};
-	
-	pre.onkeypress = function(evt) {
-		var cmdOrCtrl = evt.metaKey || evt.ctrlKey,
-			code = evt.charCode,
-			ss = this.selectionStart,
-			se = this.selectionEnd;
-		
-		if(code && !cmdOrCtrl) {
-			var character = String.fromCharCode(code);
-			
-			this.undoManager.action({
-				add: character,
-				del: ss === se? '' : this.textContent.slice(ss, se),
-				start: ss
-			});
-		}
-	};
-	
-	pre.oncut = function() {
-		ss = this.selectionStart,
-		se = this.selectionEnd,
-		selection = ss === se? '': this.textContent.slice(ss, se);
-		
-		if(selection) {
-			this.undoManager.action({
-				add: '',
-				del: selection,
-				start: ss
-			});
-			
-			gist.saved = false;
-		}
-	};
-	
-	pre.onpaste = function() {
-		var that = this,
-			ss = this.selectionStart,
-			se = this.selectionEnd,
-			selection = ss === se? '': this.textContent.slice(ss, se);
-			
-		gist.saved = false;
-			
-		setTimeout(function(){
-			var newse = that.selectionEnd,
-				pasted = that.textContent.slice(ss, newse);
-
-			that.undoManager.action({
-				add: pasted,
-				del: selection,
-				start: ss
-			});
-
-			that.innerHTML = that.innerHTML
-								.replace(/<br\b.*?>|(<div\b.*?>)+/gi, '\n')
-								.replace(/<\/div>/gi, '')
-								.replace(/&nbsp;/gi, ' ');
-			
-			ss += pasted.length;
-			
-			that.setSelectionRange(ss, ss);
-			
-			that.onkeyup();
-		}, 10);
-	};
-	
-	pre.onkeyup = function(evt) {
-		var keyCode = evt && evt.keyCode || 0,
-			code = this.textContent,
-			id = this.id;
-
-		if([
-			9, 91, 93, 16, 17, 18, // modifiers
-			20, // caps lock
-			13, // Enter (handled by keydown)
-			112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, // F[0-12]
-			27 // Esc
-		].indexOf(keyCode) > -1) {
-			return;
-		}
-		
-		if(keyCode !== 37 && keyCode !== 39) {
-			var ss = this.selectionStart,
-				se = this.selectionEnd;
-		
-			Highlight.init(this);
-			
-			// Dirty fix to #2
-			if(!/\n$/.test(code)) {
-				this.innerHTML = this.innerHTML + '\n';
-			}
-
-			if(ss !== null || se !== null) {
-				this.setSelectionRange(ss, se);
-			}
-			
-			if(id === 'css') {
-				document.title = Dabblet.title(code) + ' ✿ dabblet.com';
-			
-				Dabblet.update.CSS(code);
-			}
-			else {
-				Dabblet.update.HTML(code);
-			}
-			
-			if(keyCode) {
-				gist.saved = false;
-			}
-		}
-		
-		this.onclick();
-	};
-	
-	pre.onclick = function(evt) {
-		if(!self.Previewer) {
-			return;
-		}
-		
-		if(this.id !== 'css') { return; }
-		
-		var selection = getSelection();
-		
-		if(selection.rangeCount) {
-			var range = selection.getRangeAt(0),
-				element = range.startContainer;
-			
-			if(element.nodeType == 3) {
-				element = element.parentNode;
-			}
-			
-			var type = Previewer.get(element);
-			
-			if(type) {
-				Previewer.active = element;
-				Previewer.s[type].token = element;
-			}
-			else {
-				Previewer.hideAll();
-				Previewer.active = null;
-			}
-		}
-	}
-	
-	pre.onblur = function() {
-		if(!gist.saved) {
-			// Save draft
-			localStorage['dabblet.css'] = css.textContent;
-			localStorage['dabblet.html'] = html.textContent;
-		}
-		
-		self.Previewer && Previewer.hideAll();
-	};
-	
-	pre.onmouseover = function(evt) {
-		if(!self.Previewer) {
-			return;
-		}
-		
-		var target = evt.target,
-		    type = Previewer.get(target);
-		
-		if (type) {
-
-			var previewer = Previewer.s[type];
-			
-			if (previewer.token != target) {
-				previewer.token = target;
-				
-				target.onmouseout = function() {
-					previewer.token = this.onmouseout = null;
-					
-					// Show the previewer again on the active token
-					var active = Previewer.active;
-					
-					if (active) {
-						var type = Previewer.get(active);
-						Previewer.s[type].token = active;
-					}
-				}
-			}
-		}
-	};
+$$('.editor.page > pre').forEach(function(pre){
+	new Editor(pre);
 });
-
-css.onfocus = function() {
-	script('/code/incrementable.js', function() {
-		new Incrementable(css, function(evt) {
-			if(evt.altKey) {
-				if(evt.shiftKey) { return 10; }
-				
-				if(evt.ctrlKey) { return .1; }
-				
-				return 1;
-			}
-			
-			return 0;
-		});
-		css.onfocus = null;
-	});
-};
 
 // Note: Has to be keydown to be able to cancel the event
 document.onkeydown = function(evt) {
