47a8554d8b8b20b4e55cf24e2fa3051a41f83266
Mathias Bynens <mathias@qiwi.be>
2013-11-06 08:56:41 +0100

    Escape `&` as `&amp;`
    Previously, copying `&ampamp;` or `&amp;amp;` and pasting it into Dabblet would actually insert the text: `&amp;`. This patch fixes that.

--- code/editor.js
+++ code/editor.js
@@ -246,7 +246,7 @@ var _ = window.Editor = function(pre) {
 				
 				var pasted = evt.clipboardData.getData("text/plain");
 				
-				document.execCommand("insertHTML", false, pasted.replace(/</g, '&lt;'));
+				document.execCommand("insertHTML", false, pasted.replace(/</g, '&lt;').replace(/&/g, '&amp;'));
 				
 				that.undoManager.action({
 					add: pasted,
